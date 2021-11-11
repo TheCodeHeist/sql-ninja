d949d11728a508493135d947d904e4ae3770be26
Md. Riyasat Hossain <riyasat.hossain.29@gmail.com>
2021-11-11 11:57:43 +0600

    Furnishing the connection page, and removing useless packages for performance.

--- src/server.js
+++ src/server.js
@@ -1,13 +1,45 @@
 const express = require("express");
 const server = express();
 const path = require("path");
+const mysql = require("mysql2");
+
+let connection;
 
 server.set("views", path.join(__dirname, "views"));
 server.set("view engine", "ejs");
-server.use(express.static(__dirname + '/public'));
+server.use(express.static(__dirname + "/public"));
 
 server.get("/", (req, res) => {
   res.render("index");
+});
+
+server.post("/connect", (req, res) => {
+  if (req.body.password === "") {
+    connection = mysql.createConnection({
+      host: req.body.host,
+      user: req.body.user,
+      database: req.body.database,
+    });
+  } else {
+    connection = mysql.createConnection({
+      host: req.body.host,
+      user: req.body.user,
+      password: req.body.password,
+      database: req.body.database,
+    });
+  }
+
+  connection.connect((err) => {
+    if (err) {
+      res.render("index", {
+        error: err,
+      });
+    } else {
+      res.render("index", {
+        success: "Connected to database",
+      });
+    }
+  });
 });
 
 module.exports = server;
